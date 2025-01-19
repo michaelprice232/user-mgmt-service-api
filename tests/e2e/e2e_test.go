87fad2ade8a14c7fbb0e9b8ede34006ee9d5e46a
Michael Price <michael.price@adarga.ai>
2025-01-19 16:17:46 +0000

    Check for nil pointer ref

--- tests/e2e/e2e_test.go
+++ tests/e2e/e2e_test.go
@@ -190,7 +190,7 @@ func seedDatabase(t *testing.T, ecsClient *ecs.Client, ecsClusterName, targetSub
 
 			// Wait until the task is in a stopped state with a successful exit code
 			if describeResult.Tasks[0].LastStatus != nil && *describeResult.Tasks[0].LastStatus == "STOPPED" {
-				if *describeResult.Tasks[0].Containers[0].ExitCode == 0 {
+				if describeResult.Tasks[0].Containers[0].ExitCode != nil && *describeResult.Tasks[0].Containers[0].ExitCode == 0 {
 					t.Logf("DB seeding task completed successfully")
 					return
 				} else {
